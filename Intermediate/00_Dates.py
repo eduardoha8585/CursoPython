153fa2593e14a6ee5a60ce32326899e4598570ee
eduardoha8585 <eduardoha8585@gmail.com>
2024-04-15 20:10:03 -0500

    cambios a proyecto 00_Dates

--- Intermediate/00_Dates.py
+++ Intermediate/00_Dates.py
@@ -1,8 +1,39 @@
 ### Dates ###
 from datetime import datetime
+from datetime import time
+from datetime import date
+
+#---------------TRABAJANDO CON datetime---------------
 now = datetime.now()
-print(now.hour)
-print(now.minute)
-print(now.second)       
-print(now.month)
-print(now.day)
+
+def print_date(date):
+    print('hora',date.hour)
+    print('minutos',date.minute)
+    print('segundos',date.second)  
+    print('año', date.year)     
+    print('mes',date.month)
+    print('dia',date.day)
+    print('timestamp', date.timestamp())
+
+print_date(now)
+
+year_2024 = datetime(2024, 1, 1)
+
+print_date(year_2024)
+
+#---------------TRABAJANDO CON time---------------
+
+'''current_time = time(hour=2,minute=10,second=20) esta es una manera de rellenar time'''
+current_time = time(now.hour, now.minute, now.second)
+print('time', current_time)
+print('time_hour', current_time.hour)
+print('time_minute', current_time.minute)
+print('time_second', current_time.second)
+
+#---------------TRABAJANDO CON DATE---------------
+current_date = date(now.year, now.month, now.day)
+print('date', current_date)
+print('date_year', current_date.year)
+print('date_month', current_date.month)
+print('date_day', current_date.day)
+
